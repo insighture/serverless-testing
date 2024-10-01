4ec0188f6969ee084ec45bf73b629a1486391058
sugirjan.ragunaathan <sugirjan.ragunaathan_nh@ntucguest.com>
2024-10-01 17:21:29 +0530

    add env variable for deployment

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
   const { body } = event;
   const name = body.name || "Guest"; // Default to 'Guest' if no name is provided
-  const message = `Hello, ${name}! Welcome to AWS Lambda.`;
+  const message = `Hello, ${name}! Welcome to AWS Lambda ${process.env.ENV} environment.`;
 
   // Create a response object
   return {
